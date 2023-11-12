1d2c0ae6cd5e7297d2e92943eb041d38440234b1
rubydeve <saqibghaffar@outlook.com>
2023-11-13 03:02:02 +0400

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = "wss://chat.rent-masters.com/cable" 
   config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.allowed_request_origins = [ "http://rent-masters.com", "http://dev.rent-masters.com", "http://chat.rent-masters.com"]
+  config.action_cable.allowed_request_origins = [ "http://dev.rent-masters.com", "http://chat.rent-masters.com"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
